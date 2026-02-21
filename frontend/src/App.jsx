6126b47f3e100e8b98c8638b78da49b07c71d889
Ninjaofgames <badrhassab06@gmail.com>
2026-02-21 04:03:58 +0000

    version_210226

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SideBar from './components/sidebar'
+import Model from './pages/Model'
+import Home from './pages/Home'
+import SchoolManagement from './pages/schoolmanage'
+import Profile from './pages/profile'
+import footerComp from './components/footer'
+
+function App() {
+  return(
+    <BrowserRouter>
+      <div style={{display: "flex"}}>
+        <SideBar />
+        <main style={{flex: 1, marginLeft: "60px"}}>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/schoolM' element={<SchoolManagement />}/>
+            <Route path='/modelTrain' element={<Model />}/>
+            <Route path='/profile' element={<Profile />}/> {/* To add profile id or sth */}
+          </Routes>
+        </main>
+        <footerComp />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App
