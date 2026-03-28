845c3f3a321ae4c0d3db7353df3eb6f60797b163
marouanepro09 <marouanepro09@gmail.com>
2026-03-28 01:09:32 +0100

    Delete schools_app directory

--- schools_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'schools_app'
-    label = 'schools'
-    verbose_name = 'School Management'
